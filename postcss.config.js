a6f728023bc9d07760c685e90b76d1a3bf85b990
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2025-01-02 22:45:18 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 }
